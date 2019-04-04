aa7294ccc016d4d497d1464ff2196903e9809a42
EricJag <bajasc2@googlemail.com>
2019-04-04 17:41:49 +0200

    Merge branch 'yijia' of https://github.com/LenaHue/Hackarinos into eric
    # Conflicts:
    #	app/src/main/res/layout/home.xml
    #	app/src/main/res/values/colors.xml

--- app/src/main/java/fhdw/me/refu/Berechnung2Angestellter.java
+++ app/src/main/java/fhdw/me/refu/Berechnung2Angestellter.java
@@ -24,31 +24,37 @@ public class Berechnung2Angestellter extends Fragment {
     EditText editTextvollErwBU;
     Button buttonContinue;
 
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.berechnung_angestellte1,container,false);
+        View view = inflater.inflate(R.layout.berechnung_angestellte1, container, false);
 
         buttonContinue = view.findViewById(R.id.button_continue);
 
-        buttonContinue.setOnClickListener( new View.OnClickListener(){
+        buttonContinue.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 FragmentManager fm = getFragmentManager();
                 Fragment frag = null;
                 Class fragClass = Ergebnis_SwipeData.class;
-                try{
-                    frag = (Fragment) fragClass.newInstance();}
-                catch (Exception e){
+                try {
+                    frag = (Fragment) fragClass.newInstance();
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
 
                 FragmentTransaction ft = fm.beginTransaction();
-                ft.replace(R.id.framelayout_angestellter,frag);
+                ft.replace(R.id.framelayout_angestellter, frag);
                 ft.addToBackStack("tag").commit();
             }
         });
 
+
+
         return view;
     }
+
+
+
 }
