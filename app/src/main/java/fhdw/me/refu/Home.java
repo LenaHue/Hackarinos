55aab3fc6c9657828deae18759c757b61f6182a4
qinli3638 <msliyijia@outlook.com>
2019-04-04 16:30:10 +0200

    angestellte xml

--- app/src/main/java/fhdw/me/refu/Home.java
+++ app/src/main/java/fhdw/me/refu/Home.java
@@ -3,20 +3,27 @@ package fhdw.me.refu;
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class Home extends Fragment {
+    Button buttonBad;
+    Button buttonTuer;
+    Button buttonKueche;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState){
-
+                             Bundle savedInstanceState)
+    {
+        View view = inflater.inflate(R.layout.home, container, false);
         TextView header = getActivity().findViewById(R.id.toolbar_header);
 
         BottomNavigationView _naviagtionView = getActivity().findViewById(R.id.bottom_navigation);
@@ -28,9 +35,38 @@ public class Home extends Fragment {
 
         //Setting Menu-Item checked
         _menuItem.setChecked(true);
-
+        initButtonAndListener(view);
         //Toast.makeText(getContext(), "Internet ist nicht verfügbar.", Toast.LENGTH_SHORT).show();
-
-        return inflater.inflate(R.layout.home, container, false);
+        //toasts(view);
+        return view;
     }
+    public void initButtonAndListener(View view){
+        buttonBad = view.findViewById(R.id.button_bad);
+        buttonTuer = view.findViewById(R.id.button_tuer);
+        buttonKueche = view.findViewById(R.id.button_kueche);
+
+        buttonBad.setOnClickListener( new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "Höhe des WCs und Duschhilfe", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        buttonTuer.setOnClickListener( new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                    // Perform action on click
+                Toast.makeText(getContext(), "Höhe der Türgriffe und Breite der Türen", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        buttonKueche.setOnClickListener( new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                    // Perform action on click
+                Toast.makeText(getContext(), "unterfahrbare Arbeitsplatte und Tische", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
 }
